61e65cb4c9c0beee3652e6f06e70541b7aa1e31c
Anders Lundsby <andersls@mvb.net>
2021-09-09 09:36:40 +0200

    Ball placment

--- stikman/sketch.js
+++ stikman/sketch.js
@@ -20,7 +20,7 @@ function draw() {
   
 
   
-   translate(width / 1, height / 2);
+   translate(width / 5, height / 5);
   translate(p5.Vector.fromAngle(millis() / 600, 100));
   circle(0, 0, 20);
   
