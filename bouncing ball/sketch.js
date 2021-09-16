2eca03670e03e3b612ababc1bca9ebc8ee7970ce
Anders Lundsby <andersls@mvb.net>
2021-09-16 09:39:24 +0200

    everything

--- bouncing ball/sketch.js
+++ bouncing ball/sketch.js
@@ -1,9 +1,25 @@
+let xPos = 320;
+let yPos = 180;
+let xspeed = 5;
+let yspeed = 2;
+
+let r = 10;
+
 function setup() {
-  createCanvas(1366, 768);
+  createCanvas(1320, 626);
 }
 
 function draw() {
-  background(220);
+  background(100);
+  ellipse(xPos, yPos, r*3, r*3);
+  xPos += xspeed;
+  yPos += yspeed;
+  if (xPos > width - r || xPos < r) {
+    xspeed = -xspeed;
+  }
+  if (yPos > height - r || yPos < r) {
+    yspeed = -yspeed;
+  }
 
-  circle(200, 100, 100)
+
 }
