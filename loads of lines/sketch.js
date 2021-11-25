b9bf5ada9eba53ce0e84dcdae4cb307f2ec5e157
Anders Lundsby <andersls@mvb.net>
2021-11-25 14:33:04 +0100

    changes

--- loads of lines/sketch.js
+++ loads of lines/sketch.js
@@ -53,3 +53,31 @@ function mouseReleased() {
     lx2s.push(mouseX)
     ly2s.push(mouseY)
 }
+
+function draw() {
+    background(255)
+    for (let i = 0; i < lines; i++) {
+        if (LP == 1) {
+            line(LPx, LPy, mouseX, mouseY)
+        } else {
+
+        }
+        line(lx1s[i], ly1s[i], lx2s[i], ly2s[i])
+    }
+}
+
+function mousePressed() {
+    LP = 1
+    lines += 1
+    LPx = mouseX
+    LPy = mouseY
+    lx1s.push(mouseX)
+    ly1s.push(mouseY)
+}
+
+
+function mouseReleased() {
+    LP = 0
+    lx2s.push(mouseX)
+    ly2s.push(mouseY)
+}
