3922b7c7218c64236d5785abd2476be162573c1d
Anders Lundsby <andersls@mvb.net>
2023-01-11 10:04:55 +0100

    fix

--- python/test.py
+++ python/test.py
@@ -3,31 +3,31 @@ if input() == "y":
     print("seriøst?")
     if input() == "y" or "yes" or "ja":
         print("ring 1-1-2")
-    if input() == "n" or "no" or "nej":
+    elif input() == "n" or "no" or "nej":
         print("tror du, det er alvorligt?")
         if input() == "y" or "yes" or "ja":
             print("syntes du, forkølese er alvorligt?")
             if input() == "y" or "yes" or "ja":
                 print("ring til mor")
-            if input() == "n" or "no" or "nej":
+            elif input() == "n" or "no" or "nej":
                 print("er din læge open?")
                 if input() == "y" or "yes" or "ja":
                     print("Ring til egen læge")
-                if input() == "n" or "no" or "nej":
+                elif input() == "n" or "no" or "nej":
                     print("Ring til vagtlægen")
-        if input() == "n" or "no" or "nej":
+        elif input() == "n" or "no" or "nej":
             print("ring til din mor")
-if input() == "n" or "no" or "nej":
+elif input() == "n" or "no" or "nej":
     print("tror du, det er alvorligt?")
     if input() == "y" or "yes" or "ja":
         print("syntes du, forkølese er alvorligt?")
         if input() == "y" or "yes" or "ja":
             print("ring til mor")
-        if input() == "n" or "no" or "nej":
+        elif input() == "n" or "no" or "nej":
             print("er din læge open?")
             if input() == "y" or "yes" or "ja":
                 print("Ring til egen læge")
-            if input() == "n" or "no" or "nej":
+            elif input() == "n" or "no" or "nej":
                 print("Ring til vagtlægen")
-    if input() == "n" or "no" or "nej":
+    elif input() == "n" or "no" or "nej":
               print("ring til din mor")
